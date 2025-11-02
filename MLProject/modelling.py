d00405694c875c6d7b1d8cd17d1df254cfb15bce
Aulia Halimatusyaddiah <120395732+Awlz@users.noreply.github.com>
2025-11-02 22:55:07 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,12 +4,13 @@ import xgboost as xgb
 import pandas as pd
 from sklearn.metrics import mean_squared_error
 
-# Pastikan folder tracking ada
+# Buat folder tracking
 os.makedirs("mlruns", exist_ok=True)
 
-# Set experiment secara eksplisit
+# Set experiment
 mlflow.set_experiment("Default")
 
+# Load data
 X_train = pd.read_csv("X_train.csv")
 y_train = pd.read_csv("y_train.csv")
 X_test = pd.read_csv("X_test.csv")
@@ -20,9 +21,13 @@ with mlflow.start_run():
     model.fit(X_train, y_train)
 
     preds = model.predict(X_test)
-    rmse = mean_squared_error(y_test, preds, squared=False)
-    print(f"RMSE: {rmse}")
 
+    try:
+        rmse = mean_squared_error(y_test, preds, squared=False)
+    except TypeError:
+        rmse = mean_squared_error(y_test, preds) ** 0.5
+
+    print(f"RMSE: {rmse}")
     mlflow.log_metric("rmse", rmse)
     mlflow.xgboost.log_model(model, artifact_path="model")
 
